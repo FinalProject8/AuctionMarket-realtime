83da5278860d1984daf273beade672d516cd9f93
이승민 <iseungmin@iseungmin-ui-MacBookPro.local>
2025-04-15 12:51:26 +0900

    first commit

---
+++ src/main/java/org/example/auctionmaerketrealtime/AuctionMaerketRealtimeApplication.java
@@ -0,0 +1,13 @@
+package org.example.auctionmaerketrealtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionMaerketRealtimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionMaerketRealtimeApplication.class, args);
+    }
+
+}
